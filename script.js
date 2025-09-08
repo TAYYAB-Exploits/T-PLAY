10b4e292d8caa14a8b625396e5c9d0bd87606069
TAYYAB-Exploits <tayyabexploits@gmail.com>
2025-09-08 23:16:52 +0500

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleAbout() {
 const apps = [
   {
     name: "Termux",
-    banner: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Termux.svg/1200px-Termux.svg.png",
+    banner: "https://upload.wikimedia.org/wikipedia/commons/b/b5/Termux.svg",
     short: "Terminal emulator with Linux packages.",
     file: "app/termux.html"
   },
